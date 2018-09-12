d89830ce1696aa800f5c8855cfe9248ad9690b9f
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2018-09-12 23:41:47 +0530

    initial commit

---
+++ PPP_Practice/Source.cpp
@@ -0,0 +1,12 @@
+//this is where the main moto of the file is declared
+
+#include "std_lib_facilities.h"
+
+
+
+int main()
+{
+	cout << "Greetings programs" << endl;
+
+	return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+this solution is code summery of programming: principles and practice using C++ 2nd edition
