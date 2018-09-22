267760ddb82b6973b5ec9c2853734c5af116cdc5
Vikramaditya Singh thakur <vikram-vic@users.noreply.github.com>
2018-09-23 03:11:28 +0530

    Add files via upload

--- PPP_Practice/Source.cpp
+++ PPP_Practice/Source.cpp
@@ -2,11 +2,34 @@
 
 #include "std_lib_facilities.h"
 
+class token
+{
+public:
+	char kind;
+	int value;
+};
+
 
 
 int main()
 {
 	cout << "Greetings programs" << endl;
+
+	token t;
+	t.kind = '+';
+	cout << t.kind << endl;
+
+	token t2; 
+	t2.kind = '8';
+	t2.value = 3.14;
+
+	token tt = t;
+	cout << tt.kind << " " << tt.value << endl;
+
+	t = t2;
+	cout << t.value << endl;	
+
+
 
 	return 0;
 }
